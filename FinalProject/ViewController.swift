e83376849b5969197bbc1647ce15b504060d8397
Ozan <ozandenizones@hotmail.com>
2021-10-09 21:59:25 +0300

    Initial Commit

---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Ozan Deniz Ones on 9.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
